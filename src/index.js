cb88b996a14f757a88b1287835f5e7cdc633a55c
Matheus <matheusyagami147@gmail.com>
2021-10-02 20:37:35 -0300

    error handler middleware implemented in index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 const express = require('express')
 const routes = require('./routes')
 
+const InvalidField = require('./errors/InvalidField')
+const NotFound = require('./errors/NotFound')
+
 const app = express()
 
 app.use(express.json())
 
 app.use(routes)
+
+app.use((err, req, res, next) => {
+    let statusCode = 500
+
+    if(err instanceof NotFound) 
+        statusCode = 404
+    if(err instanceof InvalidField)
+        statusCode = 400
+        
+    res.status(statusCode).json({error: err.message})    
+})
 
 module.exports = app
