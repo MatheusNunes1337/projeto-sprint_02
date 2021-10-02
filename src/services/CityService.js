630cff4332afbe6495c99d8084c1ce494e438cc3
Matheus <matheusyagami147@gmail.com>
2021-10-02 20:21:46 -0300

    implemented the getByName method in the CityService

--- src/services/CityService.js
+++ src/services/CityService.js
@@ -1,5 +1,14 @@
+const cityModel = require('../models/city')
+
 class CityService {
-    
+    async getByName(name) {
+        return cityModel.findAll({
+            where: {
+                name: name
+            },
+            raw: true
+        })
+    }
 }
 
 module.exports = new CityService()
