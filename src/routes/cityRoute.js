3f9265aa7cbcab251c50f20e40de1b461da4e2ad
Matheus <matheusyagami147@gmail.com>
2021-10-02 19:42:55 -0300

    endpoint /cities/:name modified to /cities/name/:name and /cities/:state modified to /cities/state/:state in cityRoute.js

--- src/routes/cityRoute.js
+++ src/routes/cityRoute.js
@@ -2,7 +2,7 @@ const cityRoute = require('express').Router()
 const CityController = require('../controllers/CityController')
 
 cityRoute.post('/', CityController.create)
-cityRoute.get('/:name', CityController.getByName)
-cityRoute.get('/:state', CityController.getByState)
+cityRoute.get('/name/:name', CityController.getByName)
+cityRoute.get('/state/:state', CityController.getByState)
 
 module.exports = cityRoute
