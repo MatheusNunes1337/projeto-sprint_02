2b90e83f2b9bd3ceed1fc6d4a4e50c85e161d08c
Matheus <matheusyagami147@gmail.com>
2021-10-02 20:43:08 -0300

    method getByName in CityController is implemented

--- src/controllers/CityController.js
+++ src/controllers/CityController.js
@@ -1,3 +1,5 @@
+const NotFound = require('../errors/NotFound')
+const CityService = require('../services/CityService')
 
 class CityController {
 
@@ -5,8 +7,18 @@ class CityController {
         return res.status(201).send()
     }
      
-    async getByName(req, res) {
-        return res.status(200).send()
+    async getByName(req, res, next) {
+        try {
+            const cityName = req.params.name
+            const cities = await CityService.getByName(cityName)
+            if(cities.length === 0) {
+                throw new NotFound('city')
+            }
+
+            res.status(200).json(cities) 
+        } catch(err) {
+            next(err)
+        }
     }
 
     async getByState(req, res) {
