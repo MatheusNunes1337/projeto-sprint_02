18f82bab384b5fcda694938814f1224197681970
Matheus <matheusyagami147@gmail.com>
2021-10-02 14:17:01 -0300

    created the clients model

---
+++ src/models/user.js
@@ -0,0 +1,37 @@
+const Sequelize = require('sequelize')
+const instance = require('../database/index')
+
+const columns = {
+    fullname: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    gender: {
+        type: Sequelize.ENUM('male', 'female', 'other', 'I prefer not to say'),
+        allowNull: false
+    },
+    birthday: {
+        type: Sequelize.DATEONLY,
+        allowNull: false
+    },
+    age: {
+        type: Sequelize.NUMBER,
+        allowNull: false
+    },
+    living_in: {
+        type: Sequelize.NUMBER,
+        allowNull: false,
+        references: {
+            model: require('./city'),
+            key: 'id'
+        }
+    }
+}
+
+const options = {
+    freezeTableName: true,
+    tableName: 'clients',
+    timestamps: true
+}
+
+module.exports = instance.define('clients', columns, options)
