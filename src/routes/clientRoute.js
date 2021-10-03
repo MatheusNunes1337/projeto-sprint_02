74003ce178682dd0a0005ed6d47edacc89908533
Matheus <matheusyagami147@gmail.com>
2021-10-03 20:04:06 -0300

    checkIfCityExists and validation middlewares were implemented in client post route

--- src/routes/clientRoute.js
+++ src/routes/clientRoute.js
@@ -1,7 +1,11 @@
 const clientRoute = require('express').Router()
 const ClientController = require('../controllers/ClientController')
 
-clientRoute.post('/', ClientController.create)
+const validation = require('../middlewares/validation')
+const { createClientSchema } = require('../validations/clientValidation')
+const checkIfCityExists = require('../middlewares/checkIfCityExists')
+
+clientRoute.post('/', validation(createClientSchema), checkIfCityExists, ClientController.create)
 clientRoute.get('/:id', ClientController.getById)
 clientRoute.get('/name/:fullname', ClientController.getByFullname)
 clientRoute.patch('/:id', ClientController.updateName)
