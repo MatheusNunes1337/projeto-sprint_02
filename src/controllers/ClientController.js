04f94c52333a92f1f1da51a699466e4cf060af4f
Matheus <matheusyagami147@gmail.com>
2021-10-03 14:17:07 -0300

    ClientController and its methods are created, but any of them are implemented yet

---
+++ src/controllers/ClientController.js
@@ -0,0 +1,23 @@
+class ClientController {
+    async create(req, res) {
+        res.status(201).send()
+    }
+
+    async getByName(req, res) {
+        res.status(200).send()
+    }
+
+    async getById(req, res) {
+        res.status(200).send()
+    }
+
+    async updateName(req, res) {
+        res.status(204).send()
+    }
+
+    async delete(req, res) {
+        res.status(204).send()
+    }
+}
+
+module.exports = new ClientController()
