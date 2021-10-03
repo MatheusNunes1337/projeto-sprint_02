077093b92e6fcc9202ff1c6b25c5309fb358fa2d
Matheus <matheusyagami147@gmail.com>
2021-10-03 20:20:56 -0300

    checkIfClientExists middleware were implemented in delete route from clientRoute.js

--- src/routes/clientRoute.js
+++ src/routes/clientRoute.js
@@ -4,11 +4,12 @@ const ClientController = require('../controllers/ClientController')
 const validation = require('../middlewares/validation')
 const { createClientSchema } = require('../validations/clientValidation')
 const checkIfCityExists = require('../middlewares/checkIfCityExists')
+const checkIfClientExists = require('../middlewares/checkIfClientExists')
 
 clientRoute.post('/', validation(createClientSchema), checkIfCityExists, ClientController.create)
 clientRoute.get('/:id', ClientController.getById)
 clientRoute.get('/name/:fullname', ClientController.getByFullname)
 clientRoute.patch('/:id', ClientController.updateName)
-clientRoute.delete('/:id', ClientController.delete)
+clientRoute.delete('/:id', checkIfClientExists, ClientController.delete)
 
 module.exports = clientRoute
