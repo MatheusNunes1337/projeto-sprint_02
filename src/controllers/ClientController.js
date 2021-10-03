74597c36e8de0151dc5420159c7d161ef6996ed5
Matheus <matheusyagami147@gmail.com>
2021-10-03 14:51:26 -0300

    getmethod inside clientRoute calling ClienteController.getByFullname instead of getByName. getByFullName method of ClientController were implemented

--- src/controllers/ClientController.js
+++ src/controllers/ClientController.js
@@ -1,13 +1,24 @@
+const ClientService = require('../services/ClientService')
+
 const replaceHyphenWithSpace = require('../utils/replaceHyphenWithSpace')
+const NotFound = require('../errors/NotFound')
 
 class ClientController {
     async create(req, res) {
         res.status(201).send()
     }
 
-    async getByFullname(req, res) {
-        const fullname = replaceHyphenWithSpace(req.params.fullname)
-        res.status(200).send()
+    async getByFullname(req, res, next) {
+        try {
+            const fullname = replaceHyphenWithSpace(req.params.fullname)
+            const clients = await ClientService.getByFullname(fullname)
+            if(clients.length === 0) {
+                throw new NotFound('Client')
+            }
+            res.status(200).json(clients)
+        } catch(err) {
+            next(err)
+        }
     }
 
     async getById(req, res) {
--- src/routes/clientRoute.js
+++ src/routes/clientRoute.js
@@ -3,7 +3,7 @@ const ClientController = require('../controllers/ClientController')
 
 clientRoute.post('/', ClientController.create)
 clientRoute.get('/:id', ClientController.getById)
-clientRoute.get('/name/:fullname', ClientController.getByName)
+clientRoute.get('/name/:fullname', ClientController.getByFullname)
 clientRoute.patch('/:id', ClientController.updateName)
 clientRoute.delete('/:id', ClientController.delete)
 
